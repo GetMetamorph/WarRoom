b7826587f2661f61756ed5fd34918d6d7b496bcd
u21715994 <oussouf.youssef@gmail.com>
2023-03-02 16:12:01 +0100

    remove build

--- app/src/main/java/com/example/warroom/activities/Request.kt
+++ app/src/main/java/com/example/warroom/activities/Request.kt
@@ -4,7 +4,7 @@ class Request(val user_name: String, val category: String){
     companion object {
         fun createRequest(): List<Request>{
             var list = mutableListOf<Request>()
-            for(i in 0..20){
+            for(i in 0..20) {
                 list.add(Request("username", "Sportif"))
             }
             return list
