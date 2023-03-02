2bf1450061264f2bb93fbfff0a733fddad83c03b
Brandon-Blackstar <mor.bra34@gmail.com>
2023-03-02 20:09:52 +0100

    link home activity create challenge

--- app/src/main/java/com/example/warroom/activities/HomeActivity.kt
+++ app/src/main/java/com/example/warroom/activities/HomeActivity.kt
@@ -5,23 +5,33 @@ import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.widget.Toolbar
 import com.example.warroom.R
+import com.example.warroom.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         this.configureToolbar()
+        this.setUpUI()
     }
 
     public override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_activity_home, menu)
         return true
     }
-    
-    
+
     private fun configureToolbar() {
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+    }
+
+    private fun setUpUI() {
+        binding.iconEmpty.setOnClickListener {
+            val intent = ChallengeActivity.newIntent(this)
+            startActivity(intent)
+        }
     }
     
 }
