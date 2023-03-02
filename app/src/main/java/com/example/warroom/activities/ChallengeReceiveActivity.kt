7158069180043038549d3acab35cd2b66fba3e46
Brandon-Blackstar <mor.bra34@gmail.com>
2023-03-02 13:21:56 +0100

    update challenge receive page

---
+++ app/src/main/java/com/example/warroom/activities/ChallengeReceiveActivity.kt
@@ -0,0 +1,12 @@
+package com.example.warroom.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.warroom.R
+
+class ChallengeReceiveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_challenge_receive)
+    }
+}
